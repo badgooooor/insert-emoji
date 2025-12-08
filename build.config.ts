0535b662872bd93d2ec466712835c3b3cd9e83c8
Yuttakhan B. <yuttakhanbaingen@gmail.com>
2025-12-08 21:52:04 +0700

    Initial commit

---
+++ build.config.ts
@@ -0,0 +1,61 @@
+import { copyFile, rm } from 'node:fs/promises'
+import { join } from 'node:path'
+import { cwd } from 'node:process'
+
+import builtins from 'builtin-modules'
+
+import { defineBuildConfig } from 'unbuild'
+
+import { generateObsidianPluginManifest } from './scripts/manifest'
+import { toErrorable } from './scripts/utils'
+
+export default defineBuildConfig({
+  outDir: './dist',
+  sourcemap: true,
+  declaration: false,
+  externals: [
+    // Obsidian
+    'obsidian',
+    'electron',
+    '@codemirror/autocomplete',
+    '@codemirror/collab',
+    '@codemirror/commands',
+    '@codemirror/language',
+    '@codemirror/lint',
+    '@codemirror/search',
+    '@codemirror/state',
+    '@codemirror/view',
+    '@lezer/common',
+    '@lezer/highlight',
+    '@lezer/lr',
+    // Builtins
+    ...builtins,
+  ],
+  rollup: {
+    esbuild: {
+      format: 'cjs',
+    },
+    output: {
+      dir: './dist',
+      format: 'cjs',
+      sourcemap: 'inline',
+      entryFileNames: 'main.js',
+    },
+    // required for unocss, ofetch, etc.
+    // otherwise unbuild will detect them as external
+    // dependencies that are inline implicitly external
+    // by esbuild
+    inlineDependencies: true,
+  },
+  hooks: {
+    'build:before': async () => {
+      await toErrorable(async () => await rm(join(cwd(), 'main.js')))
+      await toErrorable(async () => await rm(join(cwd(), 'manifest.json')))
+    },
+    'build:done': async () => {
+      await generateObsidianPluginManifest()
+      await copyFile(join(cwd(), 'dist', 'main.js'), join(cwd(), 'main.js'))
+      await copyFile(join(cwd(), 'dist', 'manifest.json'), join(cwd(), 'manifest.json'))
+    },
+  },
+})
